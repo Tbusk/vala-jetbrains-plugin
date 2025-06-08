93f3f00d3ba75191a11c8058198a8344ca022a07
Trevor Busk <tbusk@myadsusa.com>
2025-06-08 00:23:18 -0400

    docs: add docs to ValaSyntaxHighlighterFactory

--- src/main/java/com/tbusk/vala_plugin/highlighting/ValaSyntaxHighlighterFactory.java
+++ src/main/java/com/tbusk/vala_plugin/highlighting/ValaSyntaxHighlighterFactory.java
@@ -7,8 +7,19 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+
+/**
+ * Factory class for creating instances of the Vala syntax highlighter.
+ * This class is used by the IntelliJ platform to provide syntax highlighting for Vala files.
+ * <br/><br/>
+ * <a href="https://plugins.jetbrains.com/docs/intellij/syntax-highlighter-and-color-settings-page.html#define-a-syntax-highlighter-factory">view documentation</a>
+ */
 public class ValaSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
 
+    /*
+     * This factory creates instances of the Vala syntax highlighter.
+     * It is used by the IntelliJ platform to provide syntax highlighting for Vala files.
+     */
     @Override
     public @NotNull SyntaxHighlighter getSyntaxHighlighter(@Nullable Project project, @Nullable VirtualFile virtualFile) {
         return new ValaSyntaxHighlighter();
