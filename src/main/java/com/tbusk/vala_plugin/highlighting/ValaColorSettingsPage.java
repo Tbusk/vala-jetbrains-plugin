c248e331194385219ca9b1e7016452881eadbf6b
Trevor Busk <tbusk@myadsusa.com>
2025-06-07 23:57:35 -0400

    docs: add docs for ValaColorSettingsPage

--- src/main/java/com/tbusk/vala_plugin/highlighting/ValaColorSettingsPage.java
+++ src/main/java/com/tbusk/vala_plugin/highlighting/ValaColorSettingsPage.java
@@ -14,8 +14,19 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import java.util.Map;
 
+/**
+ * ValaColorSettingsPage is a class that implements the ColorSettingsPage interface for Vala language support in an IDE.
+ * It provides syntax highlighting attributes, demo text, and other settings related to the appearance of Vala code.
+ * This class is used to define how Vala code should be highlighted and displayed in the editor in the settings page.
+ */
 public class ValaColorSettingsPage implements ColorSettingsPage {
 
+    /**
+     * An array of attribute descriptors that define the syntax highlighting attributes for Vala.
+     * Each descriptor corresponds to a specific type of syntax element in Vala code.
+     * The display name is shown in the color settings UI, and the Highlighter key shows as the source in the
+     * settings page.
+     */
     private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
             new AttributesDescriptor("Line comment", ValaSyntaxHighlighter.LINE_COMMENT),
             new AttributesDescriptor("Block comment", ValaSyntaxHighlighter.BLOCK_COMMENT),
@@ -34,16 +45,33 @@ public class ValaColorSettingsPage implements ColorSettingsPage {
             new AttributesDescriptor("Parentheses", ValaSyntaxHighlighter.PARENTHESES),
     };
 
+    /**
+     * Returns the icon associated with the Vala file type.
+     *
+     * @return The icon for Vala files.
+     */
     @Override
     public @Nullable Icon getIcon() {
         return ValaIcons.FILE;
     }
 
+    /**
+     * Returns the highlighter used for syntax highlighting in Vala files.
+     *
+     * @return An instance of {@link ValaSyntaxHighlighter} for Vala syntax highlighting.
+     */
     @Override
     public @NotNull SyntaxHighlighter getHighlighter() {
         return new ValaSyntaxHighlighter();
     }
 
+    /**
+     * Returns the text that serves as a demo for Vala syntax highlighting.
+     * This text is used to showcase the syntax highlighting capabilities in the settings UI and preview what code will
+     * look like when color settings are adjusted.
+     *
+     * @return A sample Vala code snippet for demonstration purposes.
+     */
     @Override
     public @NonNls @NotNull String getDemoText() {
         return """
@@ -65,21 +93,44 @@ public class ValaColorSettingsPage implements ColorSettingsPage {
                 """;
     }
 
+    /**
+     * Specifies tag-to-'color key' mapping for regions with presentation containing additional colors from color map.
+     * It's used to implement navigation between the list of keys and regions in sample editor.
+     */
     @Override
     public @Nullable Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
         return Map.of();
     }
 
+    /**
+     * Returns an array of attribute descriptors that define the syntax highlighting attributes for Vala.
+     * These descriptors are used in the color settings UI to allow users to customize the appearance of different
+     * syntax elements in Vala code.
+     *
+     * @return An array of {@link AttributesDescriptor} for Vala syntax highlighting.
+     */
     @Override
     public AttributesDescriptor @NotNull [] getAttributeDescriptors() {
         return DESCRIPTORS;
     }
 
+
+    /**
+     * Returns an array of color descriptors that define the colors used in Vala syntax highlighting.
+     * This method is typically used to provide additional color options for customization in the settings UI.
+     *
+     * @return An empty array, as Vala does not define any custom colors beyond the attributes.
+     */
     @Override
     public ColorDescriptor @NotNull [] getColorDescriptors() {
         return ColorDescriptor.EMPTY_ARRAY;
     }
 
+    /**
+     * Returns the name used in the settings / editor / color scheme section for this color settings page.
+     *
+     * @return The display name for the Vala color settings page.
+     */
     @Override
     public @NotNull @NlsContexts.ConfigurableName String getDisplayName() {
         return "Vala";
