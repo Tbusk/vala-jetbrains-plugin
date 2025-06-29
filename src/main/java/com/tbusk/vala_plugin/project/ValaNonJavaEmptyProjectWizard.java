c726cce8eb36e6bf9d05ff9b415e9fc93899be12
Trevor Busk <trevor@tbusk.com>
2025-06-29 01:13:47 -0400

    feat: add empty non-java project wizard ValaNonJavaEmptyProjectWizard
    Used in IDEs like CLion and GoLand.

--- src/main/java/com/tbusk/vala_plugin/project/ValaNonJavaEmptyProjectWizard.java
+++ src/main/java/com/tbusk/vala_plugin/project/ValaNonJavaEmptyProjectWizard.java
@@ -1,6 +1,7 @@
 package com.tbusk.vala_plugin.project;
 
 import com.intellij.facet.ui.ValidationResult;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsContexts;
@@ -9,10 +10,14 @@ import com.intellij.platform.DirectoryProjectGenerator;
 import com.tbusk.vala_plugin.language.ValaIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 
-public class ValaNonJavaEmptyProjectWizard implements DirectoryProjectGenerator {
+public class ValaNonJavaEmptyProjectWizard implements DirectoryProjectGenerator<Object> {
+    private static final Logger log = LoggerFactory.getLogger(ValaNonJavaEmptyProjectWizard.class);
+
     @Override
     public @NotNull @NlsContexts.Label String getName() {
         return "Vala project";
@@ -24,12 +29,29 @@ public class ValaNonJavaEmptyProjectWizard implements DirectoryProjectGenerator 
     }
 
     @Override
-    public void generateProject(@NotNull Project project, @NotNull VirtualFile virtualFile, @NotNull Object object, @NotNull Module module) {
-
+    public @NotNull @NlsContexts.Label String getDescription() {
+        return "Create a new Vala project";
     }
 
     @Override
-    public @NotNull ValidationResult validate(@NotNull String s) {
+    public void generateProject(@NotNull Project project, @NotNull VirtualFile virtualFile, @NotNull Object object, @NotNull Module module) {
+
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            try {
+                ValaProjectCreationUtil creationUtil = ValaProjectCreationUtil.getInstance();
+                creationUtil.setupNewProject(virtualFile, project, module);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Override
+    public @NotNull ValidationResult validate(@NotNull String name) {
+        ProjectNameValidator nameValidator = ProjectNameValidator.getInstance();
+
+        if (!nameValidator.isValid(name)) return new ValidationResult("Invalid project name");
+
         return ValidationResult.OK;
     }
 }
