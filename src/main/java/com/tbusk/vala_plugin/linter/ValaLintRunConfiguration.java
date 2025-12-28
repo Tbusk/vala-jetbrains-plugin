07ff5a0f0d49169066c420289fc4b127d83d36dd
Trevor Busk <114008066+Tbusk@users.noreply.github.com>
2025-12-28 16:11:07 -0500

    feat: add support for vala-lint (#157)
    * feat: add file templates for vala-lint
    
    * chore: bump plugin version to 1.2.7
    
    * feat: add run config for vala-lint
    
    * chore: add latest changes to CHANGELOG.md
    
    * change: tweak label/button text
    
    * change: tweak defaults / remove unused code
    
    * change: fix path issue when multiple paths are specified / path cleanup
    
    * docs: add docs for vala lint
    
    * docs: add linter support to Features.md

---
+++ src/main/java/com/tbusk/vala_plugin/linter/ValaLintRunConfiguration.java
@@ -0,0 +1,181 @@
+package com.tbusk.vala_plugin.linter;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.*;
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ProcessHandlerFactory;
+import com.intellij.execution.process.ProcessTerminatedListener;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ValaLintRunConfiguration extends RunConfigurationBase<ValaLintRunConfigurationOptions> {
+
+    private static final String DEFAULT_LINT_CONFIG_NAME = "vala-lint.conf";
+
+    protected ValaLintRunConfiguration(@NotNull Project project, @Nullable ConfigurationFactory factory, @Nullable String name) {
+        super(project, factory, name);
+    }
+
+    @NotNull
+    @Override
+    protected ValaLintRunConfigurationOptions getOptions() {
+        return (ValaLintRunConfigurationOptions) super.getOptions();
+    }
+
+    @Override
+    public @NotNull SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
+        return new ValaLintSettingsEditor();
+    }
+
+    @Override
+    public @Nullable RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment executionEnvironment) {
+        return new CommandLineState(executionEnvironment) {
+            @Override
+            protected @NotNull ProcessHandler startProcess() throws ExecutionException {
+                GeneralCommandLine commandLine = buildCommandLine();
+                OSProcessHandler processHandler = ProcessHandlerFactory.getInstance().createColoredProcessHandler(commandLine);
+                ProcessTerminatedListener.attach(processHandler);
+                return processHandler;
+            }
+        };
+    }
+
+    private GeneralCommandLine buildCommandLine() throws ExecutionException {
+        ValaLintRunConfigurationOptions options = getOptions();
+        GeneralCommandLine commandLine = new GeneralCommandLine(ValaLintCommands.DEFAULT_COMMAND);
+
+        Project project = getProject();
+
+        if (options.isDetectAndUseConfigFile()) {
+            handleDetectAndUseConfig(options, project, commandLine);
+        } else {
+            handleSpecifiedConfig(options, project, commandLine);
+        }
+
+        if (options.isAutoFix()) {
+            commandLine.addParameter(ValaLintCommands.AUTOFIX_FLAG);
+        }
+
+        String lintingMode = options.getLintingMode();
+
+        handlePaths(commandLine, lintingMode, options, project);
+
+        String projectBasePath = project.getBasePath();
+        if (projectBasePath != null) {
+            commandLine.setWorkDirectory(projectBasePath);
+        }
+
+        return commandLine;
+    }
+
+    private void handlePaths(GeneralCommandLine commandLine, String lintingMode, ValaLintRunConfigurationOptions options, Project project) throws ExecutionException {
+        if (lintingMode.equals("CHANGES_ONLY")) {
+            List<String> changedFilesPaths = getChangedFilesPaths(project);
+
+            commandLine.addParameters(changedFilesPaths);
+        } else if (lintingMode.equals("SPECIFIC_FILES")) {
+            String globPatterns = options.getCustomGlobPattern();
+
+            if (globPatterns != null && !globPatterns.trim().isEmpty()) {
+
+                String[] paths = globPatterns.trim().split(" ");
+
+                for (String path : paths) {
+                    commandLine.addParameter(path.trim());
+                }
+            }
+        }
+    }
+
+    private void handleDetectAndUseConfig(ValaLintRunConfigurationOptions options, Project project, GeneralCommandLine commandLine) throws ExecutionException {
+        Collection<VirtualFile> files = FilenameIndex.getVirtualFilesByName(DEFAULT_LINT_CONFIG_NAME, GlobalSearchScope.projectScope(project));
+
+        if (!files.isEmpty()) {
+            String foundConfigPath = files.iterator().next().getPath();
+            String relativePath = getRelativePath(foundConfigPath, project);
+            options.setConfigFilePath(relativePath);
+            commandLine.addParameter(ValaLintCommands.CONFIG_FILE_FLAG);
+            commandLine.addParameter(relativePath);
+        }
+    }
+
+    private void handleSpecifiedConfig(ValaLintRunConfigurationOptions options, Project project, GeneralCommandLine commandLine) throws ExecutionException {
+        String configPath = options.getConfigFilePath();
+
+        if (configPath != null && !configPath.trim().isEmpty()) {
+            commandLine.addParameter(ValaLintCommands.CONFIG_FILE_FLAG);
+
+            String resolvedPath = getRelativePath(configPath, project);
+
+            commandLine.addParameter(resolvedPath);
+        }
+    }
+
+    private String getRelativePath(String path, Project project) throws ExecutionException {
+        String projectPath = project.getBasePath();
+
+        if (path == null) {
+            throw new ExecutionException("Invalid path provided.");
+        }
+
+        if (projectPath == null) {
+            throw new ExecutionException("Project doesn't have a path.");
+        }
+
+        return path.replace(projectPath, ".").trim();
+    }
+
+    private List<String> getChangedFilesPaths(Project project) throws ExecutionException {
+        List<String> paths = new ArrayList<>();
+        ChangeListManager changeListManager = ChangeListManager.getInstance(getProject());
+
+        List<File> changes = new ArrayList<>(changeListManager.getAffectedPaths());
+
+        for (File file : changes) {
+
+            String changedFile = getChangedFilePath(file, project);
+            if (changedFile != null) {
+                paths.add(changedFile);
+            }
+        }
+
+        if (paths.isEmpty()) {
+            throw new ExecutionException("No Vala or Vapi files are uncommitted. ");
+        }
+
+        return paths;
+    }
+
+    private String getChangedFilePath(File file, Project project) throws ExecutionException {
+        boolean fileExists = file != null && file.exists();
+
+        if (!fileExists) {
+            throw new ExecutionException("A file you added does not exist.");
+        }
+
+        String fileName = file.getName();
+        boolean isValaExtension = fileName.endsWith(".vala");
+        boolean isVapiExtension = fileName.endsWith(".vapi");
+        boolean isValaFile = isValaExtension || isVapiExtension;
+
+        if (isValaFile) {
+            return getRelativePath(file.getPath(), project);
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/com/tbusk/vala_plugin/linter/ValaLintRunConfigurationOptions.java
@@ -0,0 +1,66 @@
+package com.tbusk.vala_plugin.linter;
+
+import com.intellij.execution.configurations.RunConfigurationOptions;
+import com.intellij.openapi.components.StoredProperty;
+
+public class ValaLintRunConfigurationOptions extends RunConfigurationOptions {
+
+    // Constants for default values
+    private static final String DEFAULT_LINTING_MODE = "CHANGES_ONLY";
+    private static final String DEFAULT_GLOB_PATTERN = "src/ test/";
+    private static final boolean DEFAULT_AUTO_FIX = false;
+    private static final boolean DEFAULT_DETECT_AND_USE_CONFIG_FILE = true;
+
+    // Constants for delegate names
+    private static final String LINTING_MODE_PROPERTY = "lintingMode";
+    private static final String CUSTOM_GLOB_PATTERN_PROPERTY = "customGlobPattern";
+    private static final String CONFIG_FILE_PATH_PROPERTY = "configFilePath";
+    private static final String AUTO_FIX_PROPERTY = "autoFix";
+    private static final String DETECT_AND_USE_CONFIG_FILE_PROPERTY = "detectAndUseConfigFile";
+
+    private final StoredProperty<String> lintingMode = string(DEFAULT_LINTING_MODE).provideDelegate(this, LINTING_MODE_PROPERTY);
+    private final StoredProperty<String> customGlobPattern = string(DEFAULT_GLOB_PATTERN).provideDelegate(this, CUSTOM_GLOB_PATTERN_PROPERTY);
+    private final StoredProperty<String> configFilePath = string("").provideDelegate(this, CONFIG_FILE_PATH_PROPERTY);
+    private final StoredProperty<Boolean> autoFix = property(DEFAULT_AUTO_FIX).provideDelegate(this, AUTO_FIX_PROPERTY);
+    private final StoredProperty<Boolean> detectAndUseConfigFile = property(DEFAULT_DETECT_AND_USE_CONFIG_FILE).provideDelegate(this, DETECT_AND_USE_CONFIG_FILE_PROPERTY);
+
+    public String getLintingMode() {
+        return lintingMode.getValue(this);
+    }
+
+    public void setLintingMode(String mode) {
+        this.lintingMode.setValue(this, mode);
+    }
+
+    public String getCustomGlobPattern() {
+        return customGlobPattern.getValue(this);
+    }
+
+    public void setCustomGlobPattern(String pattern) {
+        this.customGlobPattern.setValue(this, pattern);
+    }
+
+    public String getConfigFilePath() {
+        return configFilePath.getValue(this);
+    }
+
+    public void setConfigFilePath(String path) {
+        this.configFilePath.setValue(this, path);
+    }
+
+    public boolean isAutoFix() {
+        return autoFix.getValue(this);
+    }
+
+    public void setAutoFix(boolean fix) {
+        this.autoFix.setValue(this, fix);
+    }
+
+    public boolean isDetectAndUseConfigFile() {
+        return detectAndUseConfigFile.getValue(this);
+    }
+
+    public void setDetectAndUseConfigFile(boolean detectAndUseConfigFile) {
+        this.detectAndUseConfigFile.setValue(this, detectAndUseConfigFile);
+    }
+}
