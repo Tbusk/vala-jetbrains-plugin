1cd2816efce03a7563b2fb5383e0823dbf93ea31
Trevor Busk <trevor@tbusk.com>
2025-12-28 20:35:36 +0000

    change: tweak defaults / remove unused code

--- src/main/java/com/tbusk/vala_plugin/linter/ValaLintRunConfigurationOptions.java
+++ src/main/java/com/tbusk/vala_plugin/linter/ValaLintRunConfigurationOptions.java
@@ -7,9 +7,8 @@ public class ValaLintRunConfigurationOptions extends RunConfigurationOptions {
 
     // Constants for default values
     private static final String DEFAULT_LINTING_MODE = "CHANGES_ONLY";
-    private static final String DEFAULT_GLOB_PATTERN = "**/*.vala";
+    private static final String DEFAULT_GLOB_PATTERN = "src/ test/";
     private static final boolean DEFAULT_AUTO_FIX = false;
-    private static final String DEFAULT_VCS_MODE = "GIT";
     private static final boolean DEFAULT_DETECT_AND_USE_CONFIG_FILE = true;
 
     // Constants for delegate names
@@ -17,14 +16,12 @@ public class ValaLintRunConfigurationOptions extends RunConfigurationOptions {
     private static final String CUSTOM_GLOB_PATTERN_PROPERTY = "customGlobPattern";
     private static final String CONFIG_FILE_PATH_PROPERTY = "configFilePath";
     private static final String AUTO_FIX_PROPERTY = "autoFix";
-    private static final String VCS_CHANGE_DETECTION_MODE_PROPERTY = "vcsChangeDetectionMode";
     private static final String DETECT_AND_USE_CONFIG_FILE_PROPERTY = "detectAndUseConfigFile";
 
     private final StoredProperty<String> lintingMode = string(DEFAULT_LINTING_MODE).provideDelegate(this, LINTING_MODE_PROPERTY);
     private final StoredProperty<String> customGlobPattern = string(DEFAULT_GLOB_PATTERN).provideDelegate(this, CUSTOM_GLOB_PATTERN_PROPERTY);
     private final StoredProperty<String> configFilePath = string("").provideDelegate(this, CONFIG_FILE_PATH_PROPERTY);
     private final StoredProperty<Boolean> autoFix = property(DEFAULT_AUTO_FIX).provideDelegate(this, AUTO_FIX_PROPERTY);
-    private final StoredProperty<String> vcsChangeDetectionMode = string(DEFAULT_VCS_MODE).provideDelegate(this, VCS_CHANGE_DETECTION_MODE_PROPERTY);
     private final StoredProperty<Boolean> detectAndUseConfigFile = property(DEFAULT_DETECT_AND_USE_CONFIG_FILE).provideDelegate(this, DETECT_AND_USE_CONFIG_FILE_PROPERTY);
 
     public String getLintingMode() {
@@ -57,14 +54,6 @@ public class ValaLintRunConfigurationOptions extends RunConfigurationOptions {
 
     public void setAutoFix(boolean fix) {
         this.autoFix.setValue(this, fix);
-    }
-
-    public String getVcsChangeDetectionMode() {
-        return vcsChangeDetectionMode.getValue(this);
-    }
-
-    public void setVcsChangeDetectionMode(String mode) {
-        this.vcsChangeDetectionMode.setValue(this, mode);
     }
 
     public boolean isDetectAndUseConfigFile() {
