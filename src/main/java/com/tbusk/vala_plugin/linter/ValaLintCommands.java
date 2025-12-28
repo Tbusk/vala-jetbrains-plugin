e9bedc5cbe4bd9743eb0608e42a06abc49095c2e
Trevor Busk <trevor@tbusk.com>
2025-12-28 01:09:02 +0000

    feat: add run config for vala-lint

---
+++ src/main/java/com/tbusk/vala_plugin/linter/ValaLintCommands.java
@@ -0,0 +1,9 @@
+package com.tbusk.vala_plugin.linter;
+
+public final class ValaLintCommands {
+
+    public static final String DEFAULT_COMMAND = "io.elementary.vala-lint";
+
+    public static final String AUTOFIX_FLAG = "-f";
+    public static final String CONFIG_FILE_FLAG = "-c";
+}
